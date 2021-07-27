fcadd990f9a28ae665ea9dfdda225638ec51d189
Daniel <daniel@pop-os.localdomain>
2021-07-27 19:15:27 -0300

    feat: test01

--- haskell/test01.hs
+++ haskell/test01.hs
@@ -1,5 +1,39 @@
+{-
+ 1) a)
+    26 → pois quadrado multiplica 5 por 5, retornando 25
+    para inc que soma 1 com 25.
+
+    b)
+    36 → pois inc adiciona 1 a 5 retornando 6 a função
+    quadradro que eleva 6 ao quadrado e retorna 36.
+
+    c)
+    Erro de tipagem → pois inc retorna inteiros e
+    media recebe floats, mas com a tipagem correta seria feito a media
+    entre 4 e 6 retornando 5.
+
+  2)
+-}
 quadrado :: Floating a => a -> a
 quadrado x = x ** 2
 
 quartaPotencia :: Floating a => a -> a
 quartaPotencia x = quadrado (quadrado x)
+
+{-
+  3)
+-}
+segundosParaHoras :: Floating a => a -> a
+segundosParaHoras s = s / 60 / 60
+
+{-
+  4)
+-}
+segundosParaMinutos :: Floating a => a -> a
+segundosParaMinutos s = (segundosParaHoras s) * 60
+
+{-
+  5)
+-}
+valoresLogicos :: Bool -> Bool -> Bool
+valoresLogicos p q = (p || q) && not(p && q)
